0d6032fd8ab974006fa01ae64a8357d4f00b9ff3
Santiago Matalonga <smatalonga@gmail.com>
2018-11-24 16:30:55 +0000

    Initial Plots
    Initial plots created. Need to rework the TestScript in Java for consistency in datasets

--- DataAnalysis/ShareableDA_DataAnalysis/AnalyseLinkedListVsSlist.R
+++ DataAnalysis/ShareableDA_DataAnalysis/AnalyseLinkedListVsSlist.R
@@ -1,19 +1,43 @@
 source("CommonFunctions.r")
+library("ggplot2")
+library(reshape2)
 
-LoadDataSet <- function(){
+LoadDataSet <- function(filename){
   working_directory = getwd()
   
-  print(working_directory)
+    fileName = paste(getwd(), filename, sep="/")
+  print(fileName)
+   
+  columns <- c("TestCaseID", "UniqueId", "TimeDifference", "TotalMemory", "FreeMemory" )
   
-  
-  # fileName = paste(getwd(), "FindBugsExtraction.csv", sep="/")
-  # print(fileName)
-  # 
-  # columns <- c("Category", "BugKind", "RuleName", "BugRank", "Project", "Observations" )
-  # 
-  # 
-  # dataSet <- loadFromXLCSV(fileName, TRUE, columns)
+  dataSet <- loadFromXLCSV(fileName, TRUE, columns)
   # return(dataSet)
 }
 
-LoadDataSet()
+SetUpDataSet <- function(){
+  LinkedListDataSet <- LoadDataSet("LinkedListTestOutput.csv")
+  SListDataSet <- LoadDataSet("SListTestOutput.csv")
+  
+  LinkedListDataSet$DA <- rep("LinkedList",nrow(LinkedListDataSet))
+  SListDataSet$DA <- rep("SList",nrow(SListDataSet))
+  
+  fullDataSet<- rbind(LinkedListDataSet, SListDataSet)
+  print(fullDataSet)
+
+}
+
+plotExecutionTime <- function(fullDataSet){
+  #subsetting to first experiment
+  newData <- fullDataSet[ which( (fullDataSet$TestCaseID=="LinkedList 100 insert") |
+                                (fullDataSet$TestCaseID=="SList 100 insert")
+                                ), ]
+  
+  
+  #meltdf <- melt(newData,id="DA")
+  p <- ggplot(newData,aes(x=UniqueId,y=TimeDifference,colour=DA,group=DA)) + geom_line()
+  print(p)
+  
+}
+
+fullDataSet <- SetUpDataSet()
+plotExecutionTime(fullDataSet)
