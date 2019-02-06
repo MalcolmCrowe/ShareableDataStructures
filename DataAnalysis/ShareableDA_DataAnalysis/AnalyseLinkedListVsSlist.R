7021813ff5fb4af3675a3d8db00c1a1f2f99b488
smatalonga <35964418+smatalonga@users.noreply.github.com>
2019-02-06 16:28:41 +0000

    Updated R scripts
    Updated R scripts

--- DataAnalysis/ShareableDA_DataAnalysis/AnalyseLinkedListVsSlist.R
+++ DataAnalysis/ShareableDA_DataAnalysis/AnalyseLinkedListVsSlist.R
@@ -1,6 +1,8 @@
-source("CommonFunctions.r")
-library("ggplot2")
+library(ggplot2)
 library(reshape2)
+
+
+source("CommonFunctions.r")
 
 LoadDataSet <- function(filename){
   working_directory = getwd()
@@ -8,10 +10,10 @@ LoadDataSet <- function(filename){
     fileName = paste(getwd(), filename, sep="/")
   print(fileName)
    
-  columns <- c("TestCaseID", "Cases", "TimeDifference", "TotalMemory", "FreeMemory" )
+  columns <- c("TestCaseID", "CaseCounter", "TimeDifference", "TotalMemory", "FreeMemory" )
   
   dataSet <- loadFromXLCSV(fileName, TRUE, columns)
-  # return(dataSet)
+  return(dataSet)
 }
 
 SetUpDataSet <- function(){
@@ -34,7 +36,7 @@ plotInsertExecutionTime <- function(fullDataSet){
   
   
   #meltdf <- melt(newData,id="DA")
-  p <- ggplot(newData,aes(x=Cases,y=TimeDifference,colour=DA,group=DA)) + geom_line() +
+  p <- ggplot(newData,aes(x=CaseCounter,y=TimeDifference,colour=DA,group=DA)) + geom_line() +
     ggtitle("100 Inserts") + xlab("number of elements")
     
   print(p)
@@ -44,7 +46,7 @@ plotInsertExecutionTime <- function(fullDataSet){
   ), ]
   
   
-  p <- ggplot(newData,aes(x=Cases,y=TimeDifference,colour=DA,group=DA)) + geom_line() +
+  p <- ggplot(newData,aes(x=CaseCounter,y=TimeDifference,colour=DA, group=DA)) + geom_line() +
     ggtitle("1.000 Inserts") + xlab("number of elements")
   
   print(p)
@@ -65,7 +67,7 @@ plotInsertExecutionTime <- function(fullDataSet){
   ), ]
   
   
-  p <- ggplot(newData,aes(x=Cases,y=TimeDifference,colour=DA,group=DA)) + geom_line() +
+  p <- ggplot(newData,aes(x=CaseCounter,y=TimeDifference,colour=DA,group=DA)) + geom_line() +
     ggtitle("10.000 insert") + xlab("number of elements")
   
   print(p)
@@ -79,6 +81,33 @@ plotRemoveExecutionTime <- function(fullDataSet){
                                    (fullDataSet$TestCaseID=="Remove elements in 100")
   ), ]
   
+ 
+  
+  dfm = melt(newData, id.vars=list("CaseCounter", "DA"), measure.vars="TimeDifference")
+ 
+ 
+ p <- ggplot(dfm,aes(x=CaseCounter,y=value,colour=DA,fill=DA, group=DA)) + scale_fill_hue(l=40, c=35) + 
+    geom_bar(stat="identity", position = "dodge") +
+    ggtitle("Remove times") + xlab("number of elements") +
+   # scale_x_discrete(limits = c(0,25, 50, 100))
+    scale_x_continuous(limits = c(0,115), breaks = c(25,50,75,100))
+  
+  print(p)
+}
+
+plotFindElementsIn100 <- function(fulldataSet){
+  #subsetting to first experiment
+  newData <- fullDataSet[ which( (fullDataSet$TestCaseID=="Find elements in 100") |
+                                   (fullDataSet$TestCaseID=="Find elements in 100")
+  ), ]
+  
+  p <- ggplot(newData,aes(x=CaseCounter,y=TimeDifference,colour=DA,group=DA)) + 
+    geom_bar(stat="identity", position = "dodge") +
+    ggtitle("Find times") + xlab("number of elements") +
+    # scale_x_discrete(limits = c(0,25, 50, 100))
+    scale_x_continuous(limits = c(0,125), breaks = c(25,50,75,100))
+  
+  print(p)
   
 }
 
