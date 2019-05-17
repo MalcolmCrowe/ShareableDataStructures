5327ade7c1d8f9af29d153c14b4ff8490316e1c9
MalcolmCrowe <Malcolm.Crowe@uws.ac.uk>
2019-05-17 08:04:40 +0100

    Added DictDemo

---
+++ DictDemo/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace DictDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.memInsert = new System.Windows.Forms.RadioButton();
+            this.memRem = new System.Windows.Forms.RadioButton();
+            this.compSuccess = new System.Windows.Forms.RadioButton();
+            this.compInsert = new System.Windows.Forms.RadioButton();
+            this.compRem = new System.Windows.Forms.RadioButton();
+            this.compFailed = new System.Windows.Forms.RadioButton();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Max";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(86, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(117, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "100";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(247, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "N";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(276, 19);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(99, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(230, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 24);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "10000";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Steps10000_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(28, 112);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(747, 313);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1_Paint);
+            // 
+            // memInsert
+            // 
+            this.memInsert.AutoSize = true;
+            this.memInsert.Checked = true;
+            this.memInsert.Location = new System.Drawing.Point(531, 17);
+            this.memInsert.Name = "memInsert";
+            this.memInsert.Size = new System.Drawing.Size(73, 17);
+            this.memInsert.TabIndex = 8;
+            this.memInsert.TabStop = true;
+            this.memInsert.Tag = "0";
+            this.memInsert.Text = "memInsert";
+            this.memInsert.UseVisualStyleBackColor = true;
+            this.memInsert.CheckedChanged += new System.EventHandler(this.Radio_CheckedChanged);
+            // 
+            // memRem
+            // 
+            this.memRem.AutoSize = true;
+            this.memRem.Location = new System.Drawing.Point(531, 40);
+            this.memRem.Name = "memRem";
+            this.memRem.Size = new System.Drawing.Size(87, 17);
+            this.memRem.TabIndex = 8;
+            this.memRem.Tag = "2";
+            this.memRem.Text = "memRemove";
+            this.memRem.UseVisualStyleBackColor = true;
+            this.memRem.CheckedChanged += new System.EventHandler(this.Radio_CheckedChanged);
+            // 
+            // compSuccess
+            // 
+            this.compSuccess.AutoSize = true;
+            this.compSuccess.Location = new System.Drawing.Point(531, 63);
+            this.compSuccess.Name = "compSuccess";
+            this.compSuccess.Size = new System.Drawing.Size(98, 17);
+            this.compSuccess.TabIndex = 8;
+            this.compSuccess.Tag = "4";
+            this.compSuccess.Text = "successSearch";
+            this.compSuccess.UseVisualStyleBackColor = true;
+            this.compSuccess.CheckedChanged += new System.EventHandler(this.Radio_CheckedChanged);
+            // 
+            // compInsert
+            // 
+            this.compInsert.AutoSize = true;
+            this.compInsert.Location = new System.Drawing.Point(632, 17);
+            this.compInsert.Name = "compInsert";
+            this.compInsert.Size = new System.Drawing.Size(77, 17);
+            this.compInsert.TabIndex = 8;
+            this.compInsert.Tag = "1";
+            this.compInsert.Text = "compInsert";
+            this.compInsert.UseVisualStyleBackColor = true;
+            this.compInsert.CheckedChanged += new System.EventHandler(this.Radio_CheckedChanged);
+            // 
+            // compRem
+            // 
+            this.compRem.AutoSize = true;
+            this.compRem.Location = new System.Drawing.Point(632, 40);
+            this.compRem.Name = "compRem";
+            this.compRem.Size = new System.Drawing.Size(91, 17);
+            this.compRem.TabIndex = 8;
+            this.compRem.Tag = "3";
+            this.compRem.Text = "compRemove";
+            this.compRem.UseVisualStyleBackColor = true;
+            this.compRem.CheckedChanged += new System.EventHandler(this.Radio_CheckedChanged);
+            // 
+            // compFailed
+            // 
+            this.compFailed.AutoSize = true;
+            this.compFailed.Location = new System.Drawing.Point(632, 63);
+            this.compFailed.Name = "compFailed";
+            this.compFailed.Size = new System.Drawing.Size(84, 17);
+            this.compFailed.TabIndex = 8;
+            this.compFailed.Tag = "5";
+            this.compFailed.Text = "failedSearch";
+            this.compFailed.UseVisualStyleBackColor = true;
+            this.compFailed.CheckedChanged += new System.EventHandler(this.Radio_CheckedChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(385, 56);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(128, 57);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Max*10";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(361, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Y";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(28, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.compSuccess);
+            this.Controls.Add(this.compFailed);
+            this.Controls.Add(this.compRem);
+            this.Controls.Add(this.compInsert);
+            this.Controls.Add(this.memRem);
+            this.Controls.Add(this.memInsert);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton memInsert;
+        private System.Windows.Forms.RadioButton memRem;
+        private System.Windows.Forms.RadioButton compSuccess;
+        private System.Windows.Forms.RadioButton compInsert;
+        private System.Windows.Forms.RadioButton compRem;
+        private System.Windows.Forms.RadioButton compFailed;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ DictDemo/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Shareable;
+
+namespace DictDemo
+{
+    public partial class Form1 : Form
+    {
+        // we will add instrumentation to dict
+        public SDict<int, int> dict = SDict<int, int>.Empty;
+        public int max; // keys will fall in range 0..max
+        // we will build accumulators for a number of costs
+        public SDict<int, decimal>
+            // insert 
+            memInsertion, compInsertion,
+            // remove
+            memRemove, compRemove,
+            // ssearch
+            compSuccessSearch, compFailedSearch;
+        int rTag;
+        public Random rnd = new Random(0);
+        public int N => dict.Length ?? 0;
+        public void Step()
+        {
+            var x = rnd.Next();
+            x = x % max;
+            if (Contains(dict, x))
+                dict = Remove(dict, x);
+            else
+                dict = Add(dict, x);
+            textBox2.Text = "" + N;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(700, 313);
+            Clear();
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            max = 1000;
+            Clear();
+        }
+        private void Clear()
+        {
+            try
+            {
+                textBox1.Text = max.ToString();
+                dict = SDict<int, int>.Empty;
+                memInsertion = compInsertion = memRemove = compRemove = compSuccessSearch = compFailedSearch = SDict<int, decimal>.Empty;
+                textBox2.Text = "0";
+                Steps10000();
+            }
+            catch (Exception) { }
+        }
+
+        private void Steps10000()
+        {
+            for (var i = 0; i < 10000; i++)
+                Step();
+            pictureBox1.Invalidate();
+        }
+        private void Steps10000_Click(object sender, EventArgs e)
+        {
+            Steps10000();
+        }
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            var select = SDict<int, decimal>.Empty;
+            var display = SDict<int, decimal>.Empty;
+            e.Graphics.Clear(Color.White);
+            var br = Brushes.Black;
+            switch (rTag)
+            {
+                case 0: select = memInsertion; break;
+                case 1: select = compInsertion; break;
+                case 2: select = memRemove; break;
+                case 3: select = compRemove; break;
+                case 4: select = compSuccessSearch; break;
+                case 5: select = compFailedSearch; break;
+            }
+            var count = select.Length??0;
+            if (count == 0)
+                return;
+            var xscale = 700M / count;
+            var yscale = 0M;
+            for (var b=select.First();b!=null;b=b.Next())
+            {
+                if (b.Value.Item1 == 0)
+                    continue;
+                var x = (int)(b.Value.Item1*xscale);
+                var y = display.Contains(x) ? display[x] : 0M;
+                y += b.Value.Item2;
+                display += (x, y);
+            }
+            for (var b=display.First();b!=null;b=b.Next())
+            {
+                if (b.Value.Item1 == 0)
+                    continue;
+                var y = b.Value.Item2;
+                if (y > yscale)
+                    yscale = y;
+            } 
+            textBox3.Text = yscale.ToString();
+            yscale = yscale / 300;
+            for (var b = display.First(); b != null; b = b.Next())
+            {
+                if (b.Value.Item1 == 0)
+                    continue;
+                var y = b.Value.Item2;
+                var x = b.Value.Item1*1.0;
+                e.Graphics.FillRectangle(br, (float)x, 313-(float)(y / yscale), 2, 2);
+            }
+        }
+
+        private void Radio_CheckedChanged(object sender, EventArgs e)
+        {
+            var r = (RadioButton)sender;
+            if (r.Checked)
+                rTag = int.Parse((string)r.Tag);
+            pictureBox1.Invalidate();
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            max = max * 10;
+            textBox1.Text = "" + max;
+            Clear();
+            pictureBox1.Invalidate();
+        }
+
+        SDict<int,int> Remove(SDict<int,int> d,int x)
+        {
+            SBucket<int, int>.C = 0;
+            SBucket<int, int>.M = 0;
+            d -= x;
+            decimal c = SBucket<int, int>.C;
+            decimal m = SBucket<int, int>.M;
+            compRemove = AddIn(compRemove, c);
+            memRemove = AddIn(memRemove, m);
+            return d;
+        }
+        SDict<int,int> Add(SDict<int,int> d,int x)
+        {
+            SBucket<int, int>.C = 0;
+            SBucket<int, int>.M = 0;
+            d += (x, 1);
+            decimal c = SBucket<int, int>.C;
+            decimal m = SBucket<int, int>.M;
+            compInsertion = AddIn(compInsertion, c);
+            memInsertion = AddIn(memInsertion, m);
+            return d;
+        }
+        bool Contains(SDict<int,int> d,int x)
+        {
+            SBucket<int, int>.C = 0;
+            SBucket<int, int>.M = 0;
+            var r = d.Contains(x);
+            decimal c = SBucket<int, int>.C;
+            decimal m = SBucket<int,int>.M;
+            if (r)
+                compSuccessSearch = AddIn(compSuccessSearch, c);
+            else
+                compFailedSearch = AddIn(compFailedSearch, c);
+            return r;
+        }
+        SDict<int,decimal> AddIn(SDict<int,decimal> d,decimal c)
+        {
+            var a = d.Contains(N)?d[N]:0M;
+            a = a*0.75M + c * 0.25M;
+            return d += (N, a);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            memInsertion = compInsertion = memRemove = compRemove = compSuccessSearch = compFailedSearch = SDict<int, decimal>.Empty;
+            max = int.Parse(textBox1.Text);
+        }
+    }
+}
