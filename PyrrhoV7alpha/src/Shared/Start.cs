9fbb441394ce82bb8a13ece14c362bbbd1a17fc2
MalcolmCrowe <Malcolm.Crowe@uws.ac.uk>
2020-07-09 10:37:08 +0100

    sync

--- PyrrhoV7alpha/src/Shared/Start.cs
+++ PyrrhoV7alpha/src/Shared/Start.cs
@@ -1247,7 +1247,7 @@ namespace Pyrrho
  		internal static string[] Version = new string[]
         {
             "Pyrrho DBMS (c) 2020 Malcolm Crowe and University of the West of Scotland",
-            "7.0 alpha"," (7 July 2020)", " www.pyrrhodb.com https://pyrrhodb.uws.ac.uk"
+            "7.0 alpha"," (8 July 2020)", " www.pyrrhodb.com https://pyrrhodb.uws.ac.uk"
         };
 	}
 }
